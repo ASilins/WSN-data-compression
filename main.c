1cdcefdea1a46b80d378d41b415c35f3fadad1ee
Sarah Rešić <61244241+sresix@users.noreply.github.com>
2025-11-05 00:11:55 +0100

    Edit delta encoding

--- main.c
+++ main.c
@@ -7,16 +7,11 @@
 #define LOG_MODULE "Mini-Project - TimeSeries"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-void delta_encoding(const int16_t* timeseries_data, int16_t* differenced_data, unsigned int length) {
-    differenced_data[0] = timeseries_data[0];
+void delta_encoding(const int16_t* timeseries_data, int16_t* deltas, unsigned int length) {
+    deltas[0] = timeseries_data[0];
+    
     for(unsigned int i = 1; i < length; i++) {
-        differenced_data[i] = timeseries_data[i] - timeseries_data[i - 1];
-    }
-
-    // Perform delta encoding
-    differenced_data[0] = timeseries_data[0];
-    for(unsigned int i = 1; i < length; i++) {
-        differenced_data[i] = timeseries_data[i] - timeseries_data[i - 1];
+        deltas[i] = timeseries_data[i] - timeseries_data[i - 1];
     }
 }
 
@@ -26,13 +21,13 @@ AUTOSTART_PROCESSES(&main_process);
 PROCESS_THREAD(main_process, ev, data) {
     PROCESS_BEGIN();
 
-    int16_t* differenced_data = malloc(timeseries_length * sizeof(int16_t));
-    delta_encoding(timeseries_data, differenced_data, timeseries_length);
+    int16_t* deltas = malloc(timeseries_length * sizeof(int16_t));
+    delta_encoding(timeseries_data, deltas, timeseries_length);
 
     // Print the differenced data
-    LOG_INFO("Printing differenced data\n");
+    LOG_INFO("Printing deltas\n");
     for(unsigned int i = 0; i < timeseries_length; i++) {
-        LOG_INFO_("%d ", differenced_data[i]);
+        LOG_INFO_("%d ", deltas[i]);
 
         if ((i + 1) % 15 == 0) {
             LOG_INFO_("\n");
@@ -40,9 +35,9 @@ PROCESS_THREAD(main_process, ev, data) {
     }
     LOG_INFO_("\n");
 
-    LOG_INFO("Done printing differenced data.\n");
+    LOG_INFO("Done printing deltas.\n");
 
-    free(differenced_data);
-    
+    free(deltas);
+
     PROCESS_END();
 }
