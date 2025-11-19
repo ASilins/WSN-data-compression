0c8eb6f7930adb1ab1fc1d1bfbab02ca49b98bce
Edin Šabić <100796896+edinsabic@users.noreply.github.com>
2025-11-19 10:26:20 +0100

    Update bitpack.h

--- sprintz/bitpack.h
+++ sprintz/bitpack.h
@@ -7,18 +7,6 @@
 
 /* -------------------------------------------- */
 
-// Initialize BitWriter
-void bw_init(BitWriter* bw, uint8_t* buf, size_t cap);
-
-// Write nbits (<=32) LSBs of value into BitWriter
-bool bw_put_bits(BitWriter* bw, uint32_t value, uint8_t nbits);
-
-// Get total bytes used so far in BitWriter
-size_t bw_bytes_used(const BitWriter* bw);
-
-void br_init(BitReader* br, const uint8_t* buf, size_t cap);
-bool br_get_bits(BitReader* br, uint32_t* value_out, uint8_t nbits);
-
 // Simple bit writer: writes LSB-first into bytes
 typedef struct {
     uint8_t* buf;
@@ -35,13 +23,31 @@ typedef struct {
     uint8_t  bit_pos;   // next bit index (0..7)
 } BitReader;
 
-// Helper funtion for ZigZag encoding
-// ZigZag for 16-bit signed -> 16-bit unsigned.
-// Maps: 0 -> 0, -1 -> 1, 1 -> 2, -2 -> 3, ...
+// Initialize BitWriter
+void bw_init(BitWriter* bw, uint8_t* buf, size_t cap);
+
+// Write nbits (<=32) LSBs of value into BitWriter
+bool bw_put_bits(BitWriter* bw, uint32_t value, uint8_t nbits);
+
+// Get total bytes used so far in BitWriter
+size_t bw_bytes_used(const BitWriter* bw);
+
+// Initialize BitReader
+void br_init(BitReader* br, const uint8_t* buf, size_t cap);
+
+// Read nbits (<=32) LSBs from BitReader into value_out
+bool br_get_bits(BitReader* br, uint32_t* value_out, uint8_t nbits);
+
+/* Helper funtion for ZigZag encoding
+** ZigZag for 16-bit signed -> 16-bit unsigned.
+** Maps: 0 -> 0, -1 -> 1, 1 -> 2, -2 -> 3, ... */
 uint16_t zigzag16(int16_t v);
+
 // Inverse ZigZag (unsigned 16 -> signed 16)
 int16_t inv_zigzag16(uint16_t zz);
-// Helper function to compute minimum number of bits to represent an unsigned 16-bit integer
+
+/* Helper function to compute minimum number of bits 
+** to represent an unsigned 16-bit integer */
 uint8_t bits_required_u16(uint16_t x);
                 
 /* ============================================ */
